1c48f2528b696fbb8893d5b94337916dcf04287a
Yu-Nie <57994055+Yu-Nie@users.noreply.github.com>
2022-08-01 15:25:18 -0400

    add solution

--- assignment2.sql
+++ assignment2.sql
@@ -29,6 +29,15 @@ inner join Orders o on od.OrderID = o.OrderID
 where YEAR(o.OrderDate) - YEAR(GETDATE()) < 25 
 order by od.Quantity * od.UnitPrice
 
+-- List top 5 locations (Zip Code) where the products sold most in last 25 years.
+
+SELECT TOP 5 o.ShipPostalCode, SUM(od.Quantity) as qty 
+FROM Orders o JOIN [Order Details] od ON o.OrderID = od.OrderID 
+WHERE o.ShipPostalCode IS NOT NULL AND DATEDIFF(year, o.OrderDate, GETDATE())< 25 
+GROUP BY ShipPostalCode 
+ORDER BY qty DESC;
+
+
 --q5
 select c.City, COUNT(c.CustomerID) as NumofCustomer
 from customers c
@@ -45,6 +54,16 @@ select c.CompanyName, COUNT(o.OrderID) as NumofProduct
 from Customers c inner join orders o on c.CustomerID = o.CustomerID
 group by c.CompanyName
 
+-- Display the names of all customers  along with the  count of products they bought.
+
+SELECT c.CustomerID, c.CompanyName, c.ContactName,  SUM(od.Quantity) AS QTY 
+FROM  Customers c 
+LEFT JOIN  Orders o  ON c.CustomerID = o.CustomerID 
+LEFT JOIN  [Order Details] od ON o.OrderID = od.OrderID 
+GROUP BY c.CustomerID, c.CompanyName, c.ContactName 
+ORDER BY QTY;
+
+
 --q8
 select c.CustomerID, COUNT(od.ProductID) as NumofProduct
 from Customers c inner join Orders o on c.CustomerID = o.CustomerID
@@ -55,11 +74,14 @@ having COUNT(od.ProductID) > 100
 --q9
 select si.CompanyName as "Supplier Company Name", su.CompanyName as "Shipping Company Name"
 from shippers si cross join suppliers su
+order by 2, 1
 
 --q10
 select o.OrderDate, p.ProductName
 from Products p inner join [Order Details] od on p.ProductID = od.ProductID
 inner join Orders o on o.OrderId = od.OrderId
+group by o.OrderDate, p.ProductName 
+order by o.OrderDate
 
 --q11
 select e1.FirstName + ' ' + e1.LastName as employee1, e2.FirstName + ' ' + e1.LastName as employee2
@@ -75,6 +97,15 @@ where e.EmployeeID in (
     group by e1.ReportsTo
     having count(e1.EmployeeID) > 2
     )
+
+--Display all the Managers who have more than 2 employees reporting to them.
+
+SELECT T1.EmployeeId, T1.LastName, T1.FirstName,T2.ReportsTo, COUNT(T2.ReportsTo) AS Subordinate
+FROM Employees T1 JOIN Employees T2 ON T1.EmployeeId = T2.ReportsTo 
+WHERE T2.ReportsTo IS NOT NULL 
+GROUP BY T1.EmployeeId, T1.LastName, T1.FirstName,T2.ReportsTo 
+HAVING COUNT(T2.ReportsTo) > 2
+
 
 --q13
 select c.City, c.CompanyName, ContactName, 'Customer' as Type
@@ -103,6 +134,12 @@ select distinct c.City
 from Customers c left join Employees e on c.City = e.City
 where e.City is NULL
 
+--c
+select distinct city from Customers
+except
+select distinct city from Employees
+
+
 --q16
 select p.ProductName, sum(od.Quantity) as totalOrder
 from Products p inner join [Order Details] od on p.ProductID = od.ProductID
@@ -110,9 +147,9 @@ group by p.ProductName
 
 --q17
 --a
---I asked about how to use union with "at least 2" for this question in zoom
--- but no replies yet, so I just followed the instruction given on zoom
-select c.City
+select city from Customers
+except
+(select c.City
 from Customers c 
 group by c.City
 having COUNT(c.CustomerID) = 0
@@ -120,7 +157,7 @@ union
 select c.City
 from Customers c 
 group by c.City
-having COUNT(c.CustomerID) = 1
+having COUNT(c.CustomerID) = 1)
 
 --b
 select c.City
@@ -136,10 +173,19 @@ group by c.City
 HAVING Count(od.ProductID) >=2
 
 --q19
-select top 5 od.ProductID, od.UnitPrice as average, c.City
-from Customers c inner join Orders o on c.CustomerID = o.CustomerID
-inner join [Order Details] od on o.OrderID = od.OrderID
-order by od.Quantity desc
+select top 5 ProductID,AVG(UnitPrice) as AvgPrice,
+(select top 1 City from Customers c 
+    join Orders o on o.CustomerID = c.CustomerID 
+    join [Order Details] od2 on od2.OrderID = o.OrderID 
+    where od2.ProductID = od1.ProductID 
+    group by city 
+    order by SUM(Quantity) desc
+ ) as City
+from [Order Details] od1 
+group by ProductID
+order by sum(Quantity) desc
+
+
 
 --20
 select top 1 e.City as SoldCity, c.City as OrderCity
@@ -149,6 +195,22 @@ inner join Employees e on o.EmployeeID = e.EmployeeID
 group by e.City, c.City
 order by COUNT(o.OrderID), count(od.Quantity) desc
 
+
+select (select top 1 City 
+        from Orders o 
+        join [Order Details] od on o.OrderID=od.OrderID 
+        join Customer c on c.CustomerID = o.CustomerID 
+        group by c.CustomerID, c.City 
+        order by COUNT(*) desc
+        ) as MostOrderedCity, 
+        (select top 1 City 
+        from Orders o 
+        join [Order Details] od on o.OrderID=od.OrderID 
+        join Employees e on e.EmployeeID = o.EmployeeID 
+        group by e.EmployeeID,e.City 
+        order by sum(Quantity) desc
+        ) as MostQunatitySoldCity
+
+
 --21
--- we can use normalization to remove duplicates from a table
--- we can also use row_number() to find the duplicates rows and delete them
+-- use Row_Number() with parition by then delete the rows where rowNumber > 1
